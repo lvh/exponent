da73fdf65c3b4f4108b3af86d0da4a99050c4dbc
Laurens Van Houtven <_@lvh.cc>
2013-05-13 17:14:30 +0200

    Remove GetUserTests since it is no longer related to password auth, clean up tests some more

--- exponent/test/auth/test_password.py
+++ exponent/test/auth/test_password.py
@@ -1,46 +1,12 @@
+"""
+Tests for password-related authentication functionality.
+"""
 from axiom import errors as ae, store
-from exponent.auth import errors, password, user
+from exponent.auth import common, errors, password
 from twisted.cred import checkers, credentials, error as ce
 from twisted.internet import defer
 from twisted.trial import unittest
 from zope import interface
-
-
-class GetUserTests(unittest.TestCase):
-    """
-    Tests for getting a user by username.
-    """
-    def setUp(self):
-        self.rootStore = store.Store(self.mktemp())
-        password._UidUsernameReference(
-            store=self.rootStore,
-            uid="uid",
-            username=u"username")
-        userStore = user.User.createChildStore(self.rootStore, "uid")
-        user.User(store=userStore, uid="uid")
-
-
-    def _getUser(self, username):
-        """
-        Like regular ``_getUser``, but with this test case's store partially
-        applied.
-        """
-        return password._getUser(self.rootStore, username)
-
-
-    def test_getExistingUser(self):
-        """
-        Attempts to retrieve an existing user by username.
-        """
-        self.assertEqual(self._getUser(u"username").uid, "uid")
-
-
-    def test_getNonexistantUser(self):
-        """
-        Attempts to retrieve a user that doesn't exist.
-        """
-        self.assertRaises(ae.ItemNotFound, self._getUser, u"BOGUS")
-
 
 
 class CredentialsCheckerTests(unittest.TestCase):
@@ -51,7 +17,7 @@ class CredentialsCheckerTests(unittest.TestCase):
         self.rootStore = store.Store()
         self.userStore = store.Store()
         self.checker = password.CredentialsChecker(store=self.rootStore)
-        self.user = user.User(store=self.userStore, uid="uid")
+        self.user = common.User(store=self.userStore, uid="uid")
 
         hashedPassword = FakeUsernameHashedPassword("password")
         IUHP = credentials.IUsernameHashedPassword
