d6b03b6f8f5ef6865ac798ddb194ebae581a3aab
Laurens Van Houtven <_@lvh.cc>
2013-12-02 14:54:18 +0100

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ import sys
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
+packageName = "exponent"
+
 import re
-versionLine = open("exponent/_version.py", "rt").read()
+versionLine = open(packageName + "/_version.py", "rt").read()
 match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", versionLine, re.M)
 versionString = match.group(1)
 
@@ -19,11 +21,11 @@ class Tox(TestCommand):
         errno = tox.cmdline(self.test_args)
         sys.exit(errno)
 
-setup(name='exponent',
+setup(name=packageName,
       version=versionString,
       description='An experimental toolkit for applications with a fractal architecture',
-      long_description='An experimental toolkit for building applications with a fractal architecture, using Twisted, Axiom (object serialization wtih SQLite), AMP (an RPC protocol) and Foolscap (an object-capability library for distributed systems).',
-      url='https://github.com/lvh/exponent',
+      long_description=open("README.rst").read(),
+      url='https://github.com/lvh/' + packageName,
 
       author='Laurens Van Houtven',
       author_email='_@lvh.cc',
