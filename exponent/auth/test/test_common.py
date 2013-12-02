70d61bb6b865a301c0d7a8df8d741f6e095e5708
Laurens Van Houtven <_@lvh.cc>
2013-12-02 14:39:35 +0100

    Common auth test cleanup

--- exponent/auth/test/test_common.py
+++ exponent/auth/test/test_common.py
@@ -19,8 +19,8 @@ class UserTests(unittest.TestCase):
 
 
     def test_getUserByIdentifier(self):
-        """
-        Tests that you can get a user by uid.
+        """A user can be found by user identifier.
+
         """
         testUser = common.User.findUniqueChild(self.store, ["uid"])
         self.assertEqual(testUser, self.user)
@@ -28,40 +28,42 @@ class UserTests(unittest.TestCase):
 
 
     def test_getMissingUserByIdentifier(self):
+        """When trying to get a user that doesn't exist, an exception is
+        raised.
+
         """
-        Tests that you can get an appropriate exception when you try
-        to get a user that doesn't exist.
-        """
-        def getBogusUser():
-            return common.User.findUniqueChild(self.store, "BOGUS")
-        self.assertRaises(errors.ItemNotFound, getBogusUser)
+        self.assertRaises(errors.ItemNotFound,
+                          common.User.findUniqueChild, self.store, "BOGUS")
 
 
 
 class CreateUserTests(unittest.TestCase, commandtests.CommandTestMixin):
-    """
-    Tests for the specification of the ``CreateUser`` command.
+    """Tests for the specification of the ``CreateUser`` command.
+
     """
     command = common.CreateUser
     argumentObjects = argumentStrings = {}
     responseObjects = responseStrings = {"userIdentifier": "uid"}
+    errors = fatalErrors = {}
 
 
 
 class LogInTests(unittest.TestCase, commandtests.CommandTestMixin):
-    """
-    Tests for the specification of the ``LogIn`` command.
+    """Tests for the specification of the ``LogIn`` command.
+
     """
     command = common.LogIn
     argumentObjects = {"tokens": ["a", "b", "c"]}
     argumentStrings = {"tokens": "\x00\x01".join(["", "a", "b", "c"])}
     responseObjects = responseStrings = {}
+    errors = fatalErrors = {}
+
 
 
 class CreateIdentifierTests(unittest.TestCase):
     def test_usesSecureEntropySource(self):
-        """
-        By default, ``_createIdentifier`` uses ``os.urandom``.
+        """By default, ``_createIdentifier`` uses ``os.urandom``.
+
         """
         args, _, _, defaults = getargspec(common._createIdentifier)
         defaultEntropySource = defaults[args.index("_urandom")]
@@ -69,16 +71,16 @@ class CreateIdentifierTests(unittest.TestCase):
 
 
     def _createIdentifier(self, **kwargs):
-        """
-        Creates an identifier with a fake random number generator.
+        """Creates an identifier with a fake random number generator.
+
         """
         return common._createIdentifier(_urandom=_urandom, **kwargs)
 
 
     def test_default(self):
-        """
-        By default, ``_createIdentifier`` asks its random number generator for
-        a string 320 bits long, and encodes it as hex.
+        """By default, ``_createIdentifier`` asks its random number generator
+        for a string 320 bits long, and encodes it as hex.
+
         """
         identifier = self._createIdentifier()
         expectedLength = (320 // 8) * 2 # 320 bits, hex encoded
@@ -86,9 +88,9 @@ class CreateIdentifierTests(unittest.TestCase):
 
 
     def test_differentNumberOfBits(self):
-        """
-        ``_createIdentifier`` correctly produces identifiers of different
+        """``_createIdentifier`` correctly produces identifiers of different
         length.
+
         """
         identifier = self._createIdentifier(bits=80)
         expectedLength = (80 // 8) * 2 # 80 bits, hex encoded
@@ -97,36 +99,36 @@ class CreateIdentifierTests(unittest.TestCase):
 
 
 class FakeUrandomTests(unittest.TestCase):
-    """
-    Tests for the fake, testing-only, ``os.urandom`` stub.
+    """Tests for the ``_urandom`` fake.
+
     """
     def test_oneByte(self):
-        """
-        ``_urandom`` produces ``\x00`` when asked for a single byte.
+        """Fake ``_urandom`` produces ``\x00`` when asked for a single byte.
+
         """
         self.assertEqual(_urandom(1), "\x00")
 
 
     def test_tenBytes(self):
-        """
-        ``_urandom`` produces 10 NUL bytes when asked for 10 bytes.
+        """Fake ``_urandom`` produces 10 NUL bytes when asked for 10 bytes.
+
         """
         self.assertEqual(_urandom(10), "\x00" * 10)
 
 
 
 def _urandom(numBytes):
-    """
-    Super secure random number generator that returns as many NUL
+    """Super secure random number generator that returns as many NUL
     bytes as you ask for.
+
     """
     return "\x00" * numBytes
 
 
 
 class TokenInvalidationTests(unittest.TestCase):
-    """
-    Tests for automatic time-based token invalidation.
+    """Tests for automatic time-based token invalidation.
+
     """
     def setUp(self):
         self.now = extime.Time()
@@ -136,15 +138,16 @@ class TokenInvalidationTests(unittest.TestCase):
 
 
     def test_oneMinute(self):
-        """
-        The token validity is one minute.
+        """Tokens are valid for one minute.
+
         """
         self.assertEqual(common.Token.validity.total_seconds(), 60)
 
 
     def test_invalidate(self):
-        """
-        A token is removed from the store when the validity period has passed.
+        """A token is removed from the store when the validity period has
+        passed.
+
         """
         token = common.Token(store=self.store, source="test")
         self.now = extime.Time() + token.validity
@@ -188,21 +191,21 @@ class TokenTests(object):
 
 
 class TokenSetTests(unittest.TestCase):
-    """
-    Tests for the token set implementation.
+    """Tests for the token set implementation.
+
     """
     def test_implementsInterface(self):
-        """
-        The token set is an ``ICredentials`` as well as an ``ITokenSet``.
+        """The token set is an ``ICredentials`` as well as an ``ITokenSet``.
+
         """
         for iface in [common.ITokenSet, credentials.ICredentials]:
             self.assertTrue(iface.implementedBy(common.TokenSet))
 
 
     def test_acceptsTokens(self):
-        """
-        A token set can be instantiated with some tokens. Its identifier
+        """A token set can be instantiated with some tokens. Its identifier
         attribute will be an immutable set of the provided tokens.
+
         """
         tokenSet = common.TokenSet(["1", "2", "3"])
         self.assertEqual(tokenSet.identifiers, set(["1", "2", "3"]))
@@ -210,16 +213,16 @@ class TokenSetTests(unittest.TestCase):
 
 
     def test_noDuplicateTokenIdentifiers(self):
-        """
-        A token set can not be instantiated with duplicate tokens.
+        """A token set can not be instantiated with duplicate tokens.
+
         """
         self.assertRaises(ValueError, common.TokenSet, ["1", "1", "1"])
 
 
 
 class TokenCounterTests(unittest.TestCase):
-    """
-    Tests for the token-counting credentials checker.
+    """Tests for the token-counting credentials checker.
+
     """
     def setUp(self):
         self.store = store.Store()
@@ -229,53 +232,53 @@ class TokenCounterTests(unittest.TestCase):
 
 
     def test_requireOneTokenByDefault(self):
-        """
-        By default, the token counter requires one token.
+        """By default, the token counter requires one token.
+
         """
         self.assertEqual(self.counter.requiredTokens, 1)
 
 
     def _assertAccepts(self, identifiers):
-        """
-        Asserts that the counter accepts the given identifiers. Furthermore,
-        asserts that the identifiers are invalidated after being accepted.
+        """Asserts that the counter accepts the given identifiers.
+        Furthermore, asserts that the identifiers are invalidated
+        after being accepted.
+
         """
         d = self.counter.requestAvatarId(common.TokenSet(identifiers))
         self.assertEqual(self.successResultOf(d), self.user.identifier)
 
 
     def _assertNotAccepts(self, identifiers):
-        """
-        Asserts that the counter does not accept the given identifiers.
-        Furthermore, asserts that the identifiers are not invalidated after
-        being rejected.
+        """Asserts that the counter does not accept the given identifiers.
+        Furthermore, asserts that the identifiers are not invalidated
+        after being rejected.
+
         """
         d = self.counter.requestAvatarId(common.TokenSet(identifiers))
         self.failureResultOf(d).trap(errors.UnauthorizedLogin)
 
 
     def _countTokens(self, identifiers):
-        """
-        Counts the number of tokens in the store that have one of the given
-        identifiers.
+        """Counts the number of tokens in the store that have one of the
+        given identifiers.
+
         """
         withIdentifier = common.Token.identifier.oneOf(identifiers)
-        count = self.store.query(common.Token, withIdentifier).count()
-
+        return self.store.query(common.Token, withIdentifier).count()
 
 
     def test_acceptToken(self):
-        """
-        The token counter returns the appropriate avatar id when presented
+        """The token counter returns the appropriate avatar id when presented
         with a valid token.
+
         """
         self._assertAccepts([self.token.identifier])
 
 
     def test_acceptMultipleTokens(self):
-        """
-        The token counter returns the appropriate avatar id when requiring
+        """The token counter returns the appropriate avatar id when requiring
         two tokens and being presented with two appropriate tokens.
+
         """
         extraToken = common.Token(store=self.store, source="guesswork")
         self.assertNotEqual(extraToken.source, self.token.source)
@@ -286,9 +289,9 @@ class TokenCounterTests(unittest.TestCase):
 
 
     def test_acceptSuperfluousTokens(self):
-        """
-        The token counter accepts extra tokens, if more were provided than
+        """The token counter accepts extra tokens, if more were provided than
         are strictly necessary.
+
         """
         extraToken = common.Token(store=self.store, source="guesswork")
         identifiers = [self.token.identifier, extraToken.identifier]
@@ -296,29 +299,30 @@ class TokenCounterTests(unittest.TestCase):
 
 
     def test_dontAcceptInsufficientTokens(self):
-        """
-        The token counter does not accept if the number of tokens is
+        """The token counter does not accept if the number of tokens is
         insufficient.
+
         """
         self.counter.requiredTokens = 2
         self._assertNotAccepts([self.token.identifier])
 
 
     def test_dontAcceptInvalidTokens(self):
-        """
-        The token counter does not accept invalid token identifiers.
+        """The token counter does not accept invalid token identifiers.
+
         """
         self._assertNotAccepts(["BOGUS"])
 
 
     def test_dontAcceptDuplicateTokenTypes(self):
-        """
-        The token counter only accepts a number tokens provided the token
+        """The token counter only accepts a number tokens provided the token
         sources are unique.
 
-        Otherwise, an attacker knowing the victim's password could simply
-        generate ``N`` password authentication tokens, bypassing a user's
-        k-factor auth entirely.
+        Otherwise, an attacker that can satisfy one authentication
+        mechanism (for example: a user's password) could simply
+        generate ``N`` password authentication tokens, bypassing a
+        user's k-factor auth entirely.
+
         """
         otherToken = common.Token(store=self.store, source=self.token.source)
         self._assertNotAccepts([self.token.identifier, otherToken.identifier])
